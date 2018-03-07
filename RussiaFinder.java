d072d3494e81f66658c26be5ab5671f1c473533b
Kylewitstyle <36997469+Kylewitstyle@users.noreply.github.com>
2018-03-07 13:20:00 -0500

    add existing file

--- RussiaFinder.java
+++ RussiaFinder.java
@@ -1,10 +1,18 @@
 import java.io.*;
 import java.util.Scanner;
-
 public class RussiaFinder {
-	int count=0;
-	String fileWithRussia;
+	Scanner input = new Scanner(System.in);
 	
+//I set each count initially to negative one because my code was starting from 1 rather than zero because of the program.
+	int count1=-1;
+	int count2=-1;
+	int count3=-1;
+	String fileWithRussia;
+	String searchWord;
+	String input1;
+	String input2;
+	String input3;
+
 	RussiaFinder() {
 		fileWithRussia=" ";
 	}
@@ -12,7 +20,7 @@ public class RussiaFinder {
 boolean findRussia(String currentLine) {
 	currentLine.toLowerCase();
 	
-	if (currentLine.contains("russia")) {
+	if (currentLine.contains(searchWord)) {
 		return true;
 	}
 	else {
@@ -21,23 +29,56 @@ boolean findRussia(String currentLine) {
 
 }
 
-int countRussia() {
-count=count+1;
-	return count;
+int countWordOne() {
+count1=count1+1;
+	return count1;
+}
+
+int countWordTwo() {
+count2=count2+1;
+	return count2;
+}
+
+int countWordThree() {
+count3=count3+1;
+	return count3;
 }
 
 void read(String filetoRead) {
-	System.out.println("ready to read:"+ filetoRead);
+	
+	System.out.println("Please enter the first word that you would like to search in all lower case");
+	input1=input.nextLine();
+	
+	System.out.println("Please enter the second word that you would like to search in all lower case");
+	input2=input.nextLine();
+	
+	System.out.println("Please enter the third word that you would like to search in all lower case");
+	input3=input.nextLine();
+
 	try {
 		FileReader myfileReader=new FileReader(filetoRead);
 		BufferedReader myBufferReader= new BufferedReader(myfileReader);
 		String line=null;
 		while ((line=myBufferReader.readLine())!=null) {
+			searchWord=input1;
 			if (findRussia(line)==true) {
-				countRussia();
+				countWordOne();
 			}
+			
+			searchWord=input2;
+			if (findRussia(line)==true) {
+				countWordTwo();
+			}
+			
+			searchWord=input3;
+			if (findRussia(line)==true) {
+				countWordThree();
+			}
+
 		}
-		
+	System.out.println(input1 + " was said " + countWordOne() + " times");
+	System.out.println(input2 + " was said " + countWordTwo() + " times");
+	System.out.println(input3 + " was said " + countWordThree() + " times");
 	}
 catch (Exception ex)
 	{
@@ -45,5 +86,4 @@ catch (Exception ex)
 	
 }
 }
-
 }
